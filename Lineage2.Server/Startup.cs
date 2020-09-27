507c34d23f2f97c8ecb660e43f3b35dc054fa90c
Nihrom23 <nihrom23@gmail.com>
2020-09-27 14:54:03 +0300

    Добавил каркас стартапа

---
+++ Lineage2.Server/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lineage2.Server
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+        public void Configure()
+        {
+        }
+    }
+}
