f28f56c6288779018f3c79cb6f61f5e546037452
Nihrom23 <nihrom23@gmail.com>
2020-09-27 19:26:49 +0300

    Добавил writers для связи с логин сервисом

---
+++ Lineage2.Network/LoginService/OutWriters/LoginServicePingWriter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lineage2.Network.LoginService.OutWriters
+{
+    public class LoginServicePingWriter : OutPacketWriter
+    {
+        private readonly string Version;
+        private readonly int Build;
+
+        public override void Write()
+        {
+            WriteByte(0xA0);
+            WriteString(Version);
+            WriteInt(Build);
+        }
+    }
+}
---
+++ Lineage2.Network/LoginService/OutWriters/ServerInfoWriter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lineage2.Network.LoginService.OutWriters
+{
+    public class ServerInfoWriter : OutPacketWriter
+    {
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public string AuthCode { get; set; }
+        public bool IsGmOnly { get; set; }
+        public bool IsTestServer { get; set; }
+        public int MaxPlayers { get; set; }
+
+        public override void Write()
+        {
+            WriteByte(0xA1);
+            WriteShort(Port);
+            WriteString(Host);
+            WriteString(string.Empty);
+            WriteString(AuthCode);
+            WriteInt(0);
+            WriteShort(MaxPlayers);
+            WriteByte(IsGmOnly ? 0x01 : 0x00);
+            WriteByte(IsTestServer ? 0x01 : 0x00);
+        }
+    }
+}
