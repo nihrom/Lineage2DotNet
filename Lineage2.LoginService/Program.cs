c0db0a351675dd27055a8c8edab26dd319ded863
Nihrom23 <nihrom23@gmail.com>
2020-09-28 22:26:08 +0300

    Добавил новый проект - логин сервис

---
+++ Lineage2.LoginService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lineage2.LoginService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
