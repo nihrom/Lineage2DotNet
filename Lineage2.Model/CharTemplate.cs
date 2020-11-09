70608a0f103d6203e841b662376ffd71e2b097e1
Nihrom23 <nihrom23@gmail.com>
2020-11-10 00:25:58 +0300

    Добавил расы, классы, и базовые статы

---
+++ Lineage2.Model/CharTemplate.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lineage2.Model
+{
+    public class CharTemplate
+    {
+        public int BaseStr { get; }
+        public int BaseCon { get; }
+        public int BaseDex { get; }
+        public int BaseInt { get; }
+        public int BaseWit { get; }
+        public int BaseMen { get; }
+
+        private readonly double _baseHpMax;
+        private readonly double _baseMpMax;
+
+        public virtual double BaseHpMax(int level) => _baseHpMax;
+        public virtual double BaseMpMax(int level) => _baseMpMax;
+
+        public double BaseHpReg { get; }
+        public double BaseMpReg { get; }
+
+        public double BasePAtk { get; }
+        public double BaseMAtk { get; }
+        public double BasePDef { get; }
+        public double BaseMDef { get; }
+
+        public int BasePAtkSpd { get; }
+
+        public int BaseCritRate { get; }
+
+        public int BaseWalkSpd { get; }
+        public int BaseRunSpd { get; }
+
+        public double CollisionRadius { get; }
+        public double CollisionHeight { get; }
+
+        public CharTemplate()
+        {
+            BaseStr = 40;
+            BaseCon = 21;
+            BaseDex = 30;
+            BaseInt = 20;
+            BaseWit = 43;
+            BaseMen = 20;
+
+            _baseHpMax = 1000;
+            _baseMpMax = 700;
+
+            BaseHpReg = 1.5d;
+            BaseMpReg = 0.9d;
+
+            BasePAtk = 10;
+            BaseMAtk = 10;
+            BasePDef = 10;
+            BaseMDef = 10;
+
+            BasePAtkSpd = 300;
+
+            BaseCritRate = 4;
+
+            BaseWalkSpd = 250;
+            BaseRunSpd = 300;
+
+            CollisionRadius = 1;
+            CollisionHeight = 2;
+        }
+    }
+}
---
+++ Lineage2.Model/Enums/ClassRace.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Lineage2.Model.Enums
+{
+    public enum ClassRace
+    {
+        [Description("Human")]
+        Human,
+        [Description("Elf")]
+        Elf,
+        [Description("Dark Elf")]
+        DarkElf,
+        [Description("Orc")]
+        Orc,
+        [Description("Dwarf")]
+        Dwarf
+    }
+}
